778daf2156fcab30580912ae4ee86f75375bb74e
manummimavindya <manummi2023@gmail.com>
2026-06-22 11:44:59 +0530

    Added some features

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -21,21 +21,56 @@ export interface IOrder extends Document {
 
 const OrderSchema: Schema = new Schema(
   {
-    invoiceNumber: { type: String, unique: true, required: true },
-    userId: { type: Schema.Types.ObjectId, ref: "User", required: true }, 
-    customerName: { type: String, required: true },
-    shippingAddress: { type: String, required: true },
-    phone: { type: String, required: true },
+    invoiceNumber: { 
+      type: String, 
+      unique: true, 
+      required: true },
+
+    userId: { 
+      type: Schema.Types.ObjectId, 
+      ref: "User", 
+      required: [true, "Invoice number is required!"] }, 
+
+    customerName: { 
+      type: String, 
+      required: [true, "Customer name is required!"] },
+
+    shippingAddress: { 
+      type: String, 
+      required: [true, "Shipping address is required!"] },
+
+    phone: { 
+      type: String, 
+      required: [true, "Phone number is required!"],
+      match: [/^(?:0|94|\+94)?(?:7(0|1|2|4|5|6|7|8)\d{7})$/, "Please enter a valid Sri Lankan phone number!"] },
+    
     items: [
       {
-        plantId: { type: Schema.Types.ObjectId, ref: "plants", required: true },
-        name: { type: String, required: true },
-        quantity: { type: Number, required: true },
-        price: { type: Number, required: true }
+        plantId: { 
+          type: Schema.Types.ObjectId, 
+          ref: "plants", 
+          required: [true, "Plant ID is required!"] },
+
+        name: { 
+          type: String, 
+          required: [true, "Plant name is required!"] },
+
+        quantity: { 
+          type: Number, 
+          required: [true, "Quantity is required!"],
+          min: [1, "Quantity cannot be less than 1!"] },
+
+        price: { 
+          type: Number, 
+          required: [true, "Price is required!"],
+          min: [0, "Price cannot be negative!"] }
       }
     ],
-    totalAmount: { type: Number, required: true },
-    status: { 
+    totalAmount: { 
+      type: Number, 
+      required: true },
+    
+      status: { 
       type: String, 
       enum: ["Pending", "Processing", "Shipped", "Delivered", "Cancelled"], 
       default: "Pending" 
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -16,17 +16,38 @@ export interface IUser extends Document{
 
 const userSchema = new Schema<IUser>(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    name: { 
+      type: String, 
+      required: [true,"Name is required!"], 
+      trim: true},
+
+    email: { 
+      type: String, 
+      required: [true, "Email is required!"], 
+      unique: true, trim:true, 
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email address!"] },
+
+    password: { 
+      type: String, 
+      required: [true, "Password is required!"] },
+
     roles: {
       type: [String],
       enum: Object.values(UserRole),
       default: [UserRole.USER] 
     },
-    phone: { type: String, required: false },
-    address: { type: String, required: false }
+    phone: { 
+      type: String, 
+      required: false, 
+      trim: true, 
+      match: [/^(?:0|94|\+94)?(?:7(0|1|2|4|5|6|7|8)\d{7})$/, "Please enter a valid Sri Lankan phone number!"] },
+
+    address: { 
+      type: String, 
+      required: false, 
+      trim: true }
   },
+  
   { timestamps: true }
 );
 
